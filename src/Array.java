38d5e5e3a3f720386f04ffae2d24e2ac36364a3a
mateusdonaire <mdonaire@indracompany.com>
2020-02-20 11:31:45 -0300

    Exercício de Array

---
+++ src/Array.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] vetorA = new int [5];
+		int[] vetorB = new int [5];	
+		int[] vetorC = new int [5];	
+		
+		System.out.println("Adicione 5 valores ao vetor A");
+		System.out.println();
+		
+		for(int i = 0; i < vetorA.length; i++) {
+			System.out.print("Posição "+i+": ");
+			vetorA[i] = scan.nextInt();
+		}
+		System.out.println();
+		
+		System.out.println("Adicione 5 valores ao vetor B");
+		System.out.println();
+		
+		for(int i = 0; i < vetorB.length; i++) {
+			System.out.print("Posição "+i+": ");
+			vetorB[i] = scan.nextInt();
+		}
+		System.out.println();
+		System.out.println("A Soma entre A e B:");
+		System.out.println();
+		
+		for(int i = 0; i < vetorC.length; i++) {
+			vetorC[i] = vetorA[i] + vetorB[i];
+			System.out.println(vetorA[i]+" + "+vetorB[i]+" = "+vetorC[i]);
+		}
+		System.out.println();
+		System.out.println("A Subtração entre A e B:");
+		System.out.println();
+		
+		for(int i = 0; i < vetorC.length; i++) {
+			vetorC[i] = vetorA[i] - vetorB[i];
+			System.out.println(vetorA[i]+" - "+vetorB[i]+" = "+vetorC[i]);
+		}
+		System.out.println();
+		System.out.println("A Multiplicação entre A e B:");
+		System.out.println();
+		
+		for(int i = 0; i < vetorC.length; i++) {
+			vetorC[i] = vetorA[i] * vetorB[i];
+			System.out.println(vetorA[i]+" x "+vetorB[i]+" = "+vetorC[i]);
+		}
+		System.out.println();
+		System.out.println("A Divisão entre A e B:");
+		System.out.println();
+		
+		for(int i = 0; i < vetorC.length; i++) {
+			vetorC[i] = vetorA[i] / vetorB[i];
+			System.out.println(vetorA[i]+" / "+vetorB[i]+" = "+vetorC[i]);
+		}
+		
+	}
+}
